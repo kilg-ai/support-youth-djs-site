25c16fea4660d617692e1fa818d545b43558cec6
kilg-ai <ron.kilgore@gmail.com>
2026-07-02 15:26:40 -0400

    Add navigation data

---
+++ src/data/nav.ts
@@ -0,0 +1,9 @@
+export const navItems = [
+  { href: "/", label: "Home" },
+  { href: "/shop/", label: "Shop" },
+  { href: "/spotlight/", label: "Spotlight" },
+  { href: "/submit/", label: "Submit" },
+  { href: "/parents/", label: "Parents" },
+  { href: "/partners/", label: "Partners" },
+  { href: "/contact/", label: "Contact" }
+];
