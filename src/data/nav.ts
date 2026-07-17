8a38c3e73be77a7be39d794bd72a376e6d019272
kilg-ai <ron.kilgore@gmail.com>
2026-07-17 18:00:08 -0400

    Update global navigation structure

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -1,11 +1,40 @@
-export const navItems = [
-  { href: "/", label: "Home" },
-  { href: "/djs/", label: "Find DJs" },
-  { href: "/submit/", label: "Join the Directory" },
+export const desktopNavItems = [
+  { href: "/djs/", label: "DJs" },
   { href: "/learn/", label: "Learn" },
   { href: "/spotlight/", label: "Spotlight" },
+  { href: "/about/", label: "About" },
+  { href: "/submit/", label: "Join", primary: true }
+];
+
+export const cultureNavItems = [
   { href: "/questions/", label: "Questions & Answers" },
   { href: "/resources/", label: "Resources" },
   { href: "/parents/", label: "Parents" },
-  { href: "/about/", label: "About" }
+  { href: "/partners/", label: "Contributors" }
+];
+
+export const mobileNavGroups = [
+  {
+    title: "Explore",
+    items: [
+      { href: "/djs/", label: "Find DJs" },
+      { href: "/learn/", label: "Learn" },
+      { href: "/spotlight/", label: "Spotlight" },
+      { href: "/questions/", label: "Questions" }
+    ]
+  },
+  {
+    title: "Participate",
+    items: [
+      { href: "/submit/", label: "Join" },
+      { href: "/resources/", label: "Resources" },
+      { href: "/parents/", label: "Parents" }
+    ]
+  },
+  {
+    title: "About",
+    items: [
+      { href: "/about/", label: "About" }
+    ]
+  }
 ];
